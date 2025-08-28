017058cf12d59761cdd0b50b7792f8f260cfd20a
Patrick Hammer <patham9@gmail.com>
2025-08-28 23:48:00 +0200

    Update: support MeTTa file loading

--- src/main.pl
+++ src/main.pl
@@ -1,37 +1,22 @@
+
 :- ensure_loaded(metta).
 
-test1 :-
-    flatten_clause("(= (prog $Y) (let $X $Y
-                         (collapse (superpose (12 (plus $X 4))))))", Clause),
-    %flatten_clause("(= (prog $Y) (superpose (let $L (1 2 3)
-    %                     (collapse (superpose $L)))))", Clause),
-    assertz(Clause),
-    portray_clause(Clause),
+prologfunc(X,Y) :- Y is X+1.
 
-    findall(Result, prog(10, Result), Results),
-    format("prog(10, Results) -> Results = ~w~n", [Results]).
-
-fib_demo :-
-    register_fun(fib),
-    register_fun(minus),
-    register_fun(lt),
+prolog_interop_example :-
+    register_fun(prologfunc),
     flatten_clause(
-      "(= (fib $N)
-          (if (lt $N 2)
-              $N
-              (plus (fib (minus $N 1))
-                    (fib (minus $N 2)))))", C3),
+      "(= (mettafunc $x) (prologfunc $x))", C3),
     assertz(C3),
-    fib(30, R),
-    format("fib(30) = ~w~n", [R]).
-
-
-
-
-demo_file :- load_metta_file('./examples/fib.metta'),
-   run(R),
-   format("~w~n", [[R]]).
-
+    mettafunc(30, R),
+    format("mettafunc(30) = ~w~n", [R]).
 
 main :-
-    demo_file.
+    current_prolog_flag(argv, Args),
+    (   Args = []                         % no arguments
+    ->  prolog_interop_example
+    ;   Args = [File|_],                  % at least one argument
+        load_metta_file(File),
+        run(R),
+        format("~w~n", [[R]])
+    ).
