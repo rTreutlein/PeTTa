7710c91468ea684dacc5f0613b68fa4ead239ece
Patrick Hammer <patham9@gmail.com>
2025-08-30 16:41:08 +0200

    Update: leave main example intact

--- src/main.pl
+++ src/main.pl
@@ -5,11 +5,11 @@ prologfunc(X,Y) :- Y is X+1.
 prolog_interop_example :-
     register_fun(prologfunc),
     flatten_clause(
-      "(= (mettafunc) (let* (($x 1) ($y 2)) (+ $x $y)))", C3),
+      "(= (mettafunc $x) (prologfunc $x))", C3),
     assertz(C3),
-    listing(mettafunc).
-  %  mettafunc(30, R),
- %   format("mettafunc(30) = ~w~n", [R]).
+    listing(mettafunc),
+    mettafunc(30, R),
+    format("mettafunc(30) = ~w~n", [R]).
 
 main :-
     current_prolog_flag(argv, Args),
