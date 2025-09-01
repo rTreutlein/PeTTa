61c2283d86df22fa1b642e183de361109ee7a43c
Patrick Hammer <patham9@gmail.com>
2025-09-01 18:43:07 +0200

    Update: stdlib restructured

--- src/metta.pl
+++ src/metta.pl
@@ -1,51 +1,38 @@
-:- ensure_loaded(parser).
-:- ensure_loaded(translator).
-:- ensure_loaded(filereader).
+:- ensure_loaded([parser, translator, filereader]).
 
-%% --------- Sample semantics ---------
-test(A,B,R) :- ( A == B -> format(string(R), "is ~w, should ~w. ✅", [A,B])
-                         ; format(string(R), "is ~w, should ~w. ❌", [A,B])).
-'=='(A,B,R)    :- (A==B -> R=true ; R=false).
-'<'(A,B,R)    :- (A<B -> R=true ; R=false).
-'*'(A,B,R)   :- R is A*B.
-'/'(A,B,R)   :- R is A/B.
-'-'(A,B,R) :- R is A-B.
+%%%%%%%%%% Standard Library for MeTTa %%%%%%%%%%
+
+%Let bindings:
+'let*'([], B, B).
+'let*'([[V,Val]|Rs], B, Out) :- V = Val, 'let*'(Rs, B, Out).
+let(V,Val,In,Out) :- 'let*'([[V,Val]], In, Out).
+
+%Arithmetic & Comparison:
 '+'(A,B,R)  :- R is A+B.
-and(true,  true,  true).
-and(true,  false, false).
-and(false, true,  false).
-and(false, false, false).
-or(true,  true,  true).
-or(true,  false, true).
-or(false, true,  true).
-or(false, false, false).
+'-'(A,B,R)  :- R is A-B.
+'*'(A,B,R)  :- R is A*B.
+'/'(A,B,R)  :- R is A/B.
+'<'(A,B,R)  :- (A<B -> R=true ; R=false).
+'>'(A,B,R)  :- (A>B -> R=true ; R=false).
+'=='(A,B,R) :- (A==B -> R=true ; R=false).
+
+%Boolean Logic:
+and(true,  X, X).
+and(false, _, false).
+or( false, X, X).
+or( true,  _, true).
+not(true,  false).
+not(false, true).
+
+%Nondeterminism:
+superpose(L,X) :- member(X,L).
 empty(_) :- fail.
 
-%let/let*:
-'let*'([], Body, Body).
-'let*'([[Var,Val]|Rest], Body, Out) :- Var = Val, 'let*'(Rest, Body, Out).
-let(Var,Val,In,Out) :- 'let*'([[Var,Val]], In, Out).
+%Diagnostics / Testing:
+test(A,B,R) :- (A==B -> E='✅' ; E='❌'), format(string(R), "is ~w, should ~w. ~w", [A,B,E]).
 
-%% superpose/2: pick one element of a list on backtracking
-superpose(List, X) :- member(X, List).
-%Collapse is in flattener as it needs to inject findall
-
-%% ---------------- Demo / Setup ----------------
+%Registration:
 :- dynamic fun/1.
-register_fun(Name)   :- must_be(atom, Name), (fun(Name)->true; asserta(fun(Name))).
-unregister_fun(Name) :- retractall(fun(Name)).
-:- register_fun(superpose),
-   register_fun(empty),
-   register_fun(let),
-   register_fun('let*'),
-   register_fun('+'),
-   register_fun('-'),
-   register_fun('*'),
-   register_fun('/'),
-   register_fun('<'),
-   register_fun('=='),
-   register_fun('and'),
-   register_fun('or'),
-   register_fun('test').
-
-
+register_fun(N)   :- (fun(N)->true ; assertz(fun(N))).
+unregister_fun(N) :- retractall(fun(N)).
+:- maplist(register_fun, [superpose, empty, let, 'let*', '+','-','*','/','<','>','==', and, or, not, test]).
