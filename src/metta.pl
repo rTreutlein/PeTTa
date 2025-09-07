abf72368a5a7b1b0ab0a3e6227fc918a1c29c9c8
Patrick Hammer <patham9@gmail.com>
2025-09-07 15:58:59 +0200

    Update: efficient spaces implementation

--- src/metta.pl
+++ src/metta.pl
@@ -2,16 +2,16 @@
 
 %%%%%%%%%% Standard Library for MeTTa %%%%%%%%%%
 
-%Let bindings:
+%%% Let bindings: %%%
 'let*'([], B, B).
 'let*'([[V,Val]|Rs], B, Out) :- V = Val, 'let*'(Rs, B, Out).
 let(V,Val,In,Out) :- 'let*'([[V,Val]], In, Out).
 
-%Arithmetic & Comparison:
-'+'(A,B,R)  :- R is A+B.
-'-'(A,B,R)  :- R is A-B.
-'*'(A,B,R)  :- R is A*B.
-'/'(A,B,R)  :- R is A/B.
+%%% Arithmetic & Comparison: %%%
+'+'(A,B,R)  :- R is A + B.
+'-'(A,B,R)  :- R is A - B.
+'*'(A,B,R)  :- R is A * B.
+'/'(A,B,R)  :- R is A / B.
 '%'(A,B,R)  :- R is A mod B.
 '<'(A,B,R)  :- (A<B -> R=true ; R=false).
 '>'(A,B,R)  :- (A>B -> R=true ; R=false).
@@ -20,7 +20,7 @@ let(V,Val,In,Out) :- 'let*'([[V,Val]], In, Out).
 min(A,B,R)  :- R is min(A,B).
 max(A,B,R)  :- R is max(A,B).
 
-%Boolean Logic:
+%%% Boolean Logic: %%%
 and(true,  X, X).
 and(false, _, false).
 or( false, X, X).
@@ -28,22 +28,46 @@ or( true,  _, true).
 not(true,  false).
 not(false, true).
 
-%Nondeterminism:
+%%% Nondeterminism: %%%
 superpose(L,X) :- member(X,L).
 empty(_) :- fail.
 
-%Lists/Tuples:
+%%% Lists / Tuples: %%%
 'car-atom'([H|_], H).
 'cdr-atom'([_|T], T).
 memberfast(X, List, true) :- memberchk(X, List), !.
 memberfast(_, _, false).
 excludefast(A,L,R) :- exclude(==(A), L, R).
 
-%Diagnostics / Testing:
+%%% Diagnostics / Testing: %%%
 'trace!'(In, Content, Out) :- format('~w~n', [In]), Out = Content.
 test(A,B,R) :- (A==B -> E='✅' ; E='❌'),
                format(string(R), "is ~w, should ~w. ~w", [A,B,E]).
 
+%%% Spaces %%%
+
+ensure_dynamic_arity(Space,Arity) :- ( current_predicate(Space/Arity)
+                                       -> true ; dynamic(Space/Arity) ).
+
+'add-atom'(Space, [Rel|Args], true) :- length(Args, N), Arity is N + 2,
+                                       ensure_dynamic_arity(Space, Arity),
+                                       Term =.. [Space, Rel | Args],
+                                       assertz(Term).
+
+'remove-atom'(Space, [Rel|Args], Result) :- length(Args, N), Arity is N + 2,
+                                            ensure_dynamic_arity(Space, Arity),
+                                            Term =.. [Space, Rel | Args],
+                                            ( clause(Term, true)
+                                              -> retractall(Term),
+                                                 Result = true
+                                               ; Result = false ).
+
+%Function evaluation matches, where the unification returned true, so it unified:
+match('&self', true, Arg2, Result) :- Result=Arg2.
+
+%Match for pattern:
+match(Space, [Rel|PatArgs], OutPattern, Result) :- Term =.. [Space, Rel | PatArgs],
+                                                   Term, Result = OutPattern.
 
 %Registration:
 :- dynamic fun/1.
@@ -51,4 +75,5 @@ register_fun(N)   :- (fun(N)->true ; assertz(fun(N))).
 unregister_fun(N) :- retractall(fun(N)).
 :- maplist(register_fun, [superpose, empty, let, 'let*', '+','-','*','/', '%', min, max,
                           '<','>','==', '=', and, or, not, 'car-atom', 'cdr-atom', 'trace!', test,
-                          append, length, sort, msort, memberfast, excludefast, list_to_set]).
+                          append, length, sort, msort, memberfast, excludefast, list_to_set,
+                          'add-atom', 'remove-atom', 'match']).
