0ca02e771518cc95e01b69805440a6f56e61f285
Roman Treutlein <roman@trueagi.io>
2025-10-01 15:05:02 +0200

    Janus can't convert Variables to Python so we Convert to MeTTa SEXP in prolog

--- src/main.pl
+++ src/main.pl
@@ -15,3 +15,23 @@ main :- current_prolog_flag(argv, Args),
                        findall(R, run(default,R), Results),
                        format("~w~n", [Results]) ),
         halt.
+
+crun(ARG,OUT) :-
+  run(ARG,P),
+  convert_term(P,OUT).
+
+convert_term(Var, Str) :-
+    var(Var),
+    term_to_atom(Var, VAtom),
+    atom_concat('$', VAtom, Str).
+
+convert_term(Atom, Atom) :-
+    atom(Atom).
+
+convert_term(List, Str) :-
+    is_list(List),
+    List = [Head | Tail],
+    convert_term(Head, HStr),
+    maplist(convert_term, Tail, TStrs),
+    atomic_list_concat([HStr | TStrs], ' ', Content),
+    atomic_list_concat(['(', Content, ')'], Str).
