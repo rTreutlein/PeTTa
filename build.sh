a8633c2700c809aed9238be184ff315314a2c37a
Patrick Hammer <patham9@gmail.com>
2025-10-07 14:34:08 +0200

    Update: copy over mork_ffi

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+cp -r -f mork_ffi /home/nartech/MORK
 cd /home/nartech/MORK/kernel/
 RUSTFLAGS="-C target-cpu=native" cargo build -p mork_ffi --release
 nm -D /home/nartech/MORK/target/release/libmork_ffi.so | grep rust_mork
