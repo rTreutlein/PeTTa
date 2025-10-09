7c741e5f981cce733a4c9b8ca2714617075f37ec
Patrick Hammer <patham9@gmail.com>
2025-10-09 15:26:20 +0200

    Update: use relative paths, MORK is only expected to be in the same parent folder as PeTTa

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-cp -r -f mork_ffi /home/nartech/MORK
-cd /home/nartech/MORK/kernel/
+cp -r -f mork_ffi ../MORK
+cd ../MORK/kernel/
 RUSTFLAGS="-C target-cpu=native" cargo build -p mork_ffi --release
 nm -D /home/nartech/MORK/target/release/libmork_ffi.so | grep rust_mork
-
-cd /home/nartech/PeTTa/
+cd ../../PeTTa/
 gcc -shared -fPIC -o mylib.so main.c $(pkg-config --cflags --libs swipl)
