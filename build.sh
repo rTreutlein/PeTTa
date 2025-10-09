d3a23d906971c95bcb10cc12f646898227640cc5
Patrick Hammer <patham9@gmail.com>
2025-10-09 15:32:00 +0200

    Update: renamed to mork.c / morklib

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cd ../MORK/kernel/
 RUSTFLAGS="-C target-cpu=native" cargo build -p mork_ffi --release
 nm -D ../target/release/libmork_ffi.so | grep rust_mork
 cd ../../PeTTa/
-gcc -shared -fPIC -o mylib.so main.c $(pkg-config --cflags --libs swipl)
+gcc -shared -fPIC -o morklib.so mork.c $(pkg-config --cflags --libs swipl)
