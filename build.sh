97734c41c9aa03c47537fb1881d200a5823e17d0
Patrick Hammer <patham9@gmail.com>
2025-10-09 20:29:21 +0200

    Update: works without having to copy mork_ffi into MORK

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-cp -r -f mork_ffi ../MORK
 cd ../MORK/kernel/
+RUSTFLAGS="-C target-cpu=native" cargo build --release
+cd ../../PeTTa/mork_ffi
 RUSTFLAGS="-C target-cpu=native" cargo build -p mork_ffi --release
-nm -D ../target/release/libmork_ffi.so | grep rust_mork
-cd ../../PeTTa/
+nm -D ./target/release/libmork_ffi.so | grep rust_mork
+cd ..
 gcc -shared -fPIC -o morklib.so mork.c $(pkg-config --cflags --libs swipl)
