520031863d0fe667153279ebf8c1cfdbd936c247
Patrick Hammer <patham9@gmail.com>
2025-10-09 15:28:01 +0200

    Update: use relative paths, MORK is only expected to be in the same parent folder as PeTTa

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=/home/nartech/MORK/target/release/libmork_ffi.so swipl --stack_limit=8g -q -s ./src/main.pl -g main -- $*
+LD_PRELOAD=../MORK/target/release/libmork_ffi.so swipl --stack_limit=8g -q -s ./src/main.pl -g main -- $*
