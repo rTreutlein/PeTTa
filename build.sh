ff6ea4b5d14e5f577ddee5ec38a839ba4f7061d1
Patrick Hammer <patham9@gmail.com>
2025-10-09 23:10:51 +0200

    Update: do not explicitly build MORK here, it should already be built

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-cd ../MORK/kernel/
-RUSTFLAGS="-C target-cpu=native" cargo build --release
-cd ../../PeTTa/mork_ffi
+cd ./mork_ffi
 RUSTFLAGS="-C target-cpu=native" cargo build -p mork_ffi --release
 nm -D ./target/release/libmork_ffi.so | grep rust_mork
 gcc -shared -fPIC -o morklib.so mork.c $(pkg-config --cflags --libs swipl)
