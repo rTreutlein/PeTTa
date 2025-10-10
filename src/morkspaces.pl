3a695cc59214b23fb0a4fae347a41aa0d49ab0b6
Patrick Hammer <patham9@gmail.com>
2025-10-10 18:01:12 +0200

    Update: cleaned up further

--- src/morkspaces.pl
+++ src/morkspaces.pl
@@ -1,24 +1,21 @@
 :- multifile match/4.
 :- multifile 'add-atom'/3.
 
-% mork_query(+Pattern:list, +OutPattern:list(with one var), -Instantiated:list)
-mork_query(Pattern, OutPattern, Result) :-
-    swrite(Pattern, MorkPat),
-    mork("query", MorkPat, Temp),
-    split_string(Temp, "\n", "", Raw),
-    exclude(==(""), Raw, Lines),
-    maplist(sread, Lines, Values),
-    member(Result, Values),
-    copy_term(Pattern-OutPattern, P2-O2),
-    P2 = Result,
-    OutPattern = O2.
-
+%Add an atom to the space:
 'add-atom'('&mork', Atom, true) :- !, swrite(Atom, S),
                                    re_replace("'"/g, "", S, SClean),
                                    mork("load",SClean,B), !.
 
 %Match for pattern:
-match('&mork', Pattern, OutPattern, Result) :- !, mork_query(Pattern, OutPattern, Result).
+match('&mork', Pattern, OutPattern, Result) :- !, swrite(Pattern, MorkPat),
+                                                  mork("query", MorkPat, Temp),
+                                                  split_string(Temp, "\n", "", Raw),
+                                                  exclude(==(""), Raw, Lines),
+                                                  maplist(sread, Lines, Values),
+                                                  member(Result, Values),
+                                                  copy_term(Pattern-OutPattern, P2-O2),
+                                                  P2 = Result,
+                                                  OutPattern = O2.
 
 %Execute MM2 calculus
 'mm2-exec'('&mork', Steps, Result) :- number_string(Steps, St), mork("exec", St, A), Result=true.
@@ -39,10 +36,10 @@ match('&mork', Pattern, OutPattern, Result) :- !, mork_query(Pattern, OutPattern
 
 mork_init :- use_foreign_library('./mork_ffi/morklib.so'),
              mork("init","",A),
-             format(string(SA), "init result: ~w ~n", [A]),
+             format(string(SA), "MORK init result: ~w ~n", [A]),
              writeln(SA).
 
-mork_test :- 'mork_add-atom'('&mork', [friend,sam,tim], true),
-             'mork_add-atom'('&mork', [friend,sam,joe], true),
-             findall(C, mork_query([friend,sam,X], [friend,sam,X], C), Cs),
-             format(string(SC), "query result: ~w ~n", [Cs]), writeln(SC).
+mork_test :- 'add-atom'('&mork', [friend,sam,tim], true),
+             'add-atom'('&mork', [friend,sam,joe], true),
+             findall(C, match('&mork',[friend,sam,X], [friend,sam,X], C), Cs),
+             format(string(SC), "MORK query result: ~w ~n", [Cs]), writeln(SC).
