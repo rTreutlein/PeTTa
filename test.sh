7c63de13202b22787ad775e56763758e36906a08
Patrick Hammer <patham9@gmail.com>
2025-10-22 05:53:25 +0200

    Update: sorted

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ for f in ./examples/*; do
     ([ "$(basename "$f")" = "repl.metta" ] || [ "$(basename "$f")" = "gpt.metta" ] || [ "$(basename "$f")" = "mm2.metta" ]) && continue
     echo "Running $f"
     output=$(sh run.sh "$f" | grep "is ")
-    if ! echo "$output" | grep -q "✅"; then
+    if ! echo "$output" | grep -q "✅" || echo "$output" | grep -q "❌"; then
         echo "Failure in $f: found $output"
         exit 1
     else
