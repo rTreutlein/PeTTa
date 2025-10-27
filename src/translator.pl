7f8732c8170600d123b7a1c0ea230767d90bccb2
Patrick Hammer <patham9@gmail.com>
2025-10-27 20:03:42 +0100

    Addition: function calling ahdere to type definitions

--- src/translator.pl
+++ src/translator.pl
@@ -154,18 +154,20 @@ translate_expr([H0|T0], Goals, Out) :-
         ; translate_args(T, GsT, AVs),
           append(GsH, GsT, Inner),
           %Known function => direct call:
-          ( atom(HV), fun(HV) ->
-              % Check for type definition [:,HV,TypeChain]
-              ( catch(match('&self', [':', HV, TypeChain], TypeChain, TypeChain), _, fail)
-                -> TypeChain = [->|Xs],
-                   append(ArgTypes, [_OutType], Xs),
-                   translate_args_by_type(T, ArgTypes, GsT2, AVs2),
-                   append(GsH, GsT2, Inner2),
-                   AVsF = AVs2, InnerF = Inner2
-                 ; AVsF = AVs,  InnerF = Inner ),
-              append(AVsF, [Out], ArgsV),
-              Goal =.. [HV|ArgsV],
-              append(InnerF, [Goal], Goals)
+          ( atom(HV), fun(HV) % Check for type definition [:,HV,TypeChain]
+            -> ( catch(match('&self', [':', HV, TypeChain], TypeChain, TypeChain), _, fail)
+                 -> TypeChain = [->|Xs],
+                    append(ArgTypes, [OutType], Xs),
+                    translate_args_by_type(T, ArgTypes, GsT2, AVs2),
+                    append(GsH, GsT2, Inner2),
+                    append(AVs2, [Out], ArgsV),
+                    Goal =.. [HV|ArgsV],
+                    ( OutType == '%Undefined%'
+                      -> append(Inner2, [Goal], Goals)
+                       ;   append(Inner2, [Goal, ('get-type'(Out, OutType) ; 'get-metatype'(Out, OutType))], Goals) )
+                  ; append(AVs, [Out], ArgsV),
+                    Goal =.. [HV|ArgsV],
+                    append(Inner, [Goal], Goals) )
           %Literals (numbers, strings, etc.), known non-function atom => data:
           ; ( atomic(HV), \+ atom(HV) ; atom(HV), \+ fun(HV) ) -> Out = [HV|AVs],
                                                                   Goals = Inner
@@ -178,10 +180,14 @@ translate_expr([H0|T0], Goals, Out) :-
 
 %Selectively apply translate_args for non-Expression args while Expression args stay as data input:
 translate_args_by_type([], _, [], []) :- !.
-translate_args_by_type([A|As], [T|Ts], GsOut, [AV|AVs]) :- !, ( T == 'Expression' -> AV = A, GsA = []
-                                                                                   ; translate_expr(A, GsA, AV)),
-                                                              translate_args_by_type(As, Ts, GsRest, AVs),
-                                                              append(GsA, GsRest, GsOut).
+translate_args_by_type([A|As], [T|Ts], GsOut, [AV|AVs]) :-
+                      ( T == 'Expression' -> AV = A, GsA = []
+                                           ; translate_expr(A, GsA1, AV),
+                                             ( T == '%Undefined%'
+                                               -> GsA = GsA1
+                                                ; append(GsA1, [('get-type'(AV, T) ; 'get-metatype'(AV, T))], GsA))),
+                                             translate_args_by_type(As, Ts, GsRest, AVs),
+                                             append(GsA, GsRest, GsOut).
 
 %Handle data list:
 eval_data_term(X, [], X) :- (var(X); atomic(X)), !.
