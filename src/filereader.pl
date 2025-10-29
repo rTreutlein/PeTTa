ac71a5236df8f4cf5e4be95aedbe0139c4de22bc
Roman Treutlein <roman@trueagi.io>
2025-10-29 10:49:30 +0100

    Implement 2 pass compilation

--- src/filereader.pl
+++ src/filereader.pl
@@ -15,7 +15,10 @@ process_metta_string(S, RunArg) :- split_string(S, "\n", "", L0),
                                    string_codes(FunctionizedCode, Codes),
                                    ( phrase(top_forms(Forms), Codes)
                                      -> true ; format("Parse error: invalid or unbalanced top-level form(s).~n", []), halt(1) ),
-                                   ( maplist(assert_function, Forms)
+                                   ( maplist(parse_form, Forms, ParsedForms)
+                                     -> true ; format("Parse error: failed to parse one or more forms.~n", []), halt(1) ),
+                                   register_functions_first_pass(ParsedForms),
+                                   ( maplist(assert_function, Forms, ParsedForms)
                                      -> true ; format("Parse error: failed to process one or more forms.~n", []), halt(1) ).
 
 %Functions stay functions and runaway S-expressions become add-atom calls with result omitted:
@@ -23,27 +26,46 @@ to_function_form(T, T) :- T = [=, [_|_], _], !.
 to_function_form(T, [=, [run, default], ['add-atom','&self', T]]).
 
 %From a function string: parse, extract first atom as name, register, transform to relation, assert:
-assert_function(FormStr) :- ( sread(FormStr, Orig)
-                              -> true ; format('Parse error in form: ~w~n', [FormStr]), halt(1) ),
-                            to_function_form(Orig, Term),
-                            Term = [=, [FAtom|W], BodyExpr],
-                            ( FAtom == run, W == ['default']
-                              -> ( eval(BodyExpr, Result),
-                                   swrite(Result, S), format("~w~n", [S])
-                                   -> true ; format('Evaluation error in run form: ~w~n', [FormStr]), halt(1) )
-                               ; add_sexp('&self', Term),
-                                 atom(FAtom),
-                                 register_fun(FAtom),
-                                 translate_clause(Term, Clause),
-                                 assertz(Clause, Ref),
-                                 ( current_prolog_flag(argv, Args) -> true ; Args = [] ),
-                                 ( \+ ( member(Flag, Args), (Flag == silent ; Flag == '--silent' ; Flag == '-s') )
-                                   -> format("\e[33m-->  metta S-exp  -->~n\e[36m~w~n\e[33m--> prolog clause -->~n\e[32m", [FormStr]),
-                                      clause(Head, Body, Ref),
-                                      ( Body == true -> Show = Head ; Show = (Head :- Body) ),
-                                      portray_clause(current_output, Show),
-                                      format("\e[33m^^^^^^^^^^^^^^^^^^^^^~n\e[0m")
-                                    ; true )).
+assert_function(FormStr) :-
+    parse_form(FormStr, Term),
+    register_functions_first_pass([Term]),
+    assert_function(FormStr, Term).
+
+assert_function(FormStr, Term) :-
+    Term = [=, [FAtom|W], BodyExpr],
+    ( FAtom == run, W == ['default']
+      -> ( eval(BodyExpr, Result),
+           swrite(Result, S), format("~w~n", [S])
+           -> true ; format('Evaluation error in run form: ~w~n', [FormStr]), halt(1) )
+       ; add_sexp('&self', Term),
+         atom(FAtom),
+         register_fun(FAtom),
+         translate_clause(Term, Clause),
+         assertz(Clause, Ref),
+         ( current_prolog_flag(argv, Args) -> true ; Args = [] ),
+         ( \+ ( member(Flag, Args), (Flag == silent ; Flag == '--silent' ; Flag == '-s') )
+           -> format("\e[33m-->  metta S-exp  -->~n\e[36m~w~n\e[33m--> prolog clause -->~n\e[32m", [FormStr]),
+              clause(Head, Body, Ref),
+              ( Body == true -> Show = Head ; Show = (Head :- Body) ),
+              portray_clause(current_output, Show),
+              format("\e[33m^^^^^^^^^^^^^^^^^^^^^~n\e[0m")
+            ; true )).
+
+parse_form(FormStr, Term) :-
+    ( sread(FormStr, Orig)
+      -> to_function_form(Orig, Term)
+       ; format('Parse error in form: ~w~n', [FormStr]), fail ).
+
+register_functions_first_pass([]).
+register_functions_first_pass([[=, [run, default], _]|Rest]) :-
+    !,
+    register_functions_first_pass(Rest).
+register_functions_first_pass([[=, [FAtom|_], _]|Rest]) :-
+    atom(FAtom),
+    register_fun(FAtom),
+    register_functions_first_pass(Rest).
+register_functions_first_pass([_|Rest]) :-
+    register_functions_first_pass(Rest).
 
 %Collect characters until all parentheses are balanced (depth 0), accumulating codes:
 grab_until_balanced(D,Acc,Cs) --> [C], { ( C=0'( -> D1 is D+1 ; C=0') -> D1 is D-1 ; D1=D ), Acc1=[C|Acc] },
