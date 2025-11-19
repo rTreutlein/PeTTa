1950d687b729fc18e531bab10b232deef503ae14
Patrick Hammer <patham9@gmail.com>
2025-11-20 00:05:58 +0100

    Update: intermediate commit

--- src/specializer.pl
+++ src/specializer.pl
@@ -3,14 +3,15 @@
 %Specialize a call by creating and translating a specialized version of the MeTTa code:
 maybe_specialize_call(HV, AVs, Out, Goal) :- setup_call_cleanup( (catch(nb_getval(specsucess,Prev),_,Prev = []), nb_setval(specsucess,false) ),
                                                                  maybe_specialize_call_(HV, AVs, Out, Goal),
-                                                                 (Prev == true -> nb_setval(specsucess,Prev) ; true) ).
-maybe_specialize_call_(HV, AVs, Out, Goal) :- \+ current_function(HV), %We are compiling HV don't try to specialize it
+                                                                 (Prev == true -> nb_setval(specsucess,Prev)) ).
+maybe_specialize_call_(HV, AVs, Out, Goal) :- \+ catch(nb_getval(HV, HV), _, HV = none), %We are compiling HV don't try to specialize it
                                               catch(nb_getval(HV, MetaList0), _, fail), %Get all the info about HV
                                               copy_term(MetaList0, MetaList),           %Make a copy to specialize
                                               bind_specialized_args_meta_list(AVs,MetaList,BindSet),
                                               copy_term(BindSet,BindSetC),
                                               term_variables(BindSetC,Vars), %Create name
-                                              maplist(=(var),Vars), maplist(term_to_atom,BindSetC,BindSetAtom),
+                                              maplist(=(var),Vars),
+                                              maplist(term_to_atom,BindSetC,BindSetAtom),
                                               atomic_list_concat([HV, '_Spec_' | BindSetAtom], SpecName),
                                               maplist({HV,SpecName}/[fun_meta(Args,Body),fun_meta(Args,BodySubst)]>>substitute_nested(HV,Args,SpecName,Body,BodySubst), MetaList, MetaSubsts),
                                               ( ho_specialization(HV, SpecName)     %Previously specialzed function
@@ -21,13 +22,13 @@ maybe_specialize_call_(HV, AVs, Out, Goal) :- \+ current_function(HV), %We are c
                                                     ( %Compile new Specialization:
                                                       ( catch(match('&self', [':', HV, TypeChain], TypeChain, TypeChain), _, fail)
                                                         -> add_sexp('&self', [':', SpecName, TypeChain]) ; true ),
-                                                      current_function(PrevCurrent),
+                                                      catch(nb_getval(PrevCurrent, PrevCurrent), _, PrevCurrent = none),
                                                       call_cleanup( maplist(compile_meta_clauses(SpecName),MetaSubsts,ClauseInfos),
                                                                     ( PrevCurrent == none -> catch(nb_delete(current), _, true) ; nb_setval(current, PrevCurrent) )),
                                                       nb_getval(specsucess, true),
                                                       maplist(assert_specialization_clause(SpecName), ClauseInfos),
                                                       assertz(ho_specialization(HV, SpecName))
-                                                      -> true ; format("Fail unregister~n"),
+                                                      -> true ; format("Not specialized ~w~n", [SpecName/Arity]),
                                                                 unregister_fun(SpecName/Arity),
                                                                 retractall(arity(SpecName,Arity)), fail ))), !,
                                               append(AVs, [Out], CallArgs),
@@ -49,9 +50,6 @@ assert_specialization_clause(SpecName, clause_info(Input, Clause)) :- assertz(Cl
 compile_meta_clauses(SpecName, fun_meta(ArgsNorm, BodyExpr), clause_info(Input, Clause)) :- Input = [=, [SpecName|ArgsNorm], BodyExpr],
                                                                                             translate_clause_(Input, Clause,false).
 
-%Retrieves the function currently being compiled from thread-local storage:
-current_function(Current) :- catch(nb_getval(current, Current), _, Current = none).
-
 %Collects all higher-order variables eligible for specialization across all meta-clauses:
 bind_specialized_args_meta_list(Values, MetaList, HoBindSet) :- setof(HoVar,
                                                                       ArgsNorm^BodyExpr^HoBinds^HoBindsPerArg^
@@ -70,25 +68,24 @@ bind_specialized_args(BodyExpr, Value, Arg, HoVars) :- term_variables(Arg, Vars)
 
 %Determines which variables correspond to specializable arguments based on their usage in the body:
 bind_specialized_args_([], [], _, []).
-bind_specialized_args_([Var|Vars], [Copy|Copies], BodyExpr, HoVars) :- ( specializable_arg(Copy), (var_used_as_head(Var, BodyExpr) ; var_used_as_ho_arg(Var, BodyExpr))
-                                                                         -> Var = Copy, HoVars = [Var|RestHoVars]
+bind_specialized_args_([Var|Vars], [Copy|Copies], BodyExpr, HoVars) :- ( specializable_arg(Copy), (var_use_check(head, Var, BodyExpr) ; var_use_check(ho, Var, BodyExpr))
+                                                                         -> Var = Copy,
+                                                                            HoVars = [Var|RestHoVars]
                                                                           ; HoVars = RestHoVars ),
                                                                        bind_specialized_args_(Vars, Copies, BodyExpr, RestHoVars).
 
-%Detects whether a variable appears in head position of any nested expression:
-var_used_as_head(Var, [Head|_]) :- Var == Head, nb_setval(specsucess, true), !.
-var_used_as_head(Var, L) :- is_list(L), member(E,L), is_list(E), var_used_as_head(Var,E).
-
-%Detects whether a variable appears as a higher-order argument in any nested expression:
-var_used_as_ho_arg(Var, [Head|Args]) :- specializable_arg(Head),
-                                        member(Arg, Args),
-                                        ( Var == Arg                     % directly as argument
-                                          ; is_list(Arg),
-                                            var_used_as_ho_arg(Var, Arg) ). % or deeper inside
-var_used_as_ho_arg(Var, L) :- is_list(L),
-                              member(E,L),
-                              is_list(E),
-                              var_used_as_ho_arg(Var,E).
+%If Var appears at list head it means function call, meaning specialization is needed, and detect when used as HOL arg
+var_use_check(head, Var, [Head|_]) :- Var == Head,
+                                      nb_setval(specsucess, true).
+var_use_check(ho, Var, [Head|Args]) :- specializable_arg(Head),
+                                       member(Arg, Args),
+                                       ( Var == Arg
+                                       ; is_list(Arg),
+                                         var_use_check(ho, Var, Arg) ).
+var_use_check(Mode, Var, L) :- is_list(L),
+                               member(E, L),
+                               is_list(E),
+                               var_use_check(Mode, Var, E).
 
 %Tests whether an argument represents a specializable function or partial application:
 specializable_arg(Arg) :- nonvar(Arg), 
